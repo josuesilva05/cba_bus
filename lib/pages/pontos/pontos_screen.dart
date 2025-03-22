b54c3b4626779cc0a98dfff5fb56c9fec3aef8f0
josueoliveira1 <josue.oliveira8569@gmail.com>
2025-03-22 15:12:08 -0400

    fix: exclude google-maps depencency

--- lib/pages/pontos/pontos_screen.dart
+++ lib/pages/pontos/pontos_screen.dart
@@ -1,26 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:cba_bus/pages/pontos/map_view.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PontosScreen extends StatelessWidget {
-  final Set<Marker> _markers = {};
-
-  void _onMapCreated(GoogleMapController controller) {
-    // Aqui você pode adicionar os marcadores ou outras configurações do mapa
-    _markers.add(
-      Marker(
-        markerId: MarkerId('1'),
-        position: LatLng(-23.5505, -46.6333), // Exemplo: São Paulo
-        infoWindow: InfoWindow(title: 'São Paulo', snippet: 'Capital de SP'),
-      ),
-    );
-  }
+  const PontosScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Mapa de Pontos')),
-      body: MapView(),
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: LatLng(-15.6274065, -56.0598531),
+        initialZoom: 14.0,
+      ),
+      children: [
+        TileLayer(
+          // Bring your own tiles
+          urlTemplate:
+              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+          userAgentPackageName: 'com.example.app', // Add your app identifier
+          // And many more recommended properties!
+        ),
+        RichAttributionWidget(
+          // Include a stylish prebuilt attribution widget that meets all requirments
+          attributions: [
+            TextSourceAttribution('OpenStreetMap contributors'),
+            // Also add images...
+          ],
+        ),
+      ],
     );
   }
 }
