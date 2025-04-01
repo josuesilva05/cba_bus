6078fee49bd192dd59e9f98d57b48275aa9618a2
Josué Silva <167824520+josuesilva05@users.noreply.github.com>
2025-04-01 11:17:27 -0400

    hotfix: map link

--- lib/pages/live_bus/map_view.dart
+++ lib/pages/live_bus/map_view.dart
@@ -269,7 +269,7 @@ class _MapViewState extends State<MapView> {
               TileLayer(
                 urlTemplate:
                     isDark
-                        ? 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png'
+                        ? 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
                         : 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.example.app',
               ),
